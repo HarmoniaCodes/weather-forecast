553503009443a533724d80c5a51fe2aa75577aa9
Nick <harmoniacodes@proton.me>
2023-02-25 16:55:42 -0500

    add initial api query functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const todayWeather = document.getElementById("weatherToday");
+
+const apiKey = ""
+
+const weatherQuery = "https://api.openweathermap.org/data/2.5/forecast?q=Atlanta&appid=" + apiKey;
+
+async function getWeather() {
+    let results = await fetch(weatherQuery);
+    let data = await results.json();
+    todayWeather.innerText = data.city.name;
+    // console.log(data);
+}
+
+getWeather();
